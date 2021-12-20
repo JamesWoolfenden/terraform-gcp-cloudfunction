e9133a61dcaf024935223befba84c92b3e99e782
James Woolfenden <james.woolfenden@gmail.com>
2021-12-20 15:43:05 +0000

    refresh

--- example/examplea/google_storage_bucket.trigger.tf
+++ example/examplea/google_storage_bucket.trigger.tf
@@ -2,4 +2,5 @@ resource "google_storage_bucket" "trigger" {
   #checkov:skip=CKV_GCP_62
   name                        = "examplea-function-1"
   uniform_bucket_level_access = true
+  location                    = "eu"
 }
